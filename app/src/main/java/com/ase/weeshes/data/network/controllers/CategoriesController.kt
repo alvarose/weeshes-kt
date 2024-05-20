34efb601b387451127f07ec4f9ea2fb34b1ba048
Álvaro Serrano <alvaroserranoe@gmail.com>
2024-05-20 22:45:58 +0200

    Uncategorized products with deleted category

--- app/src/main/java/com/ase/weeshes/data/network/controllers/CategoriesController.kt
+++ app/src/main/java/com/ase/weeshes/data/network/controllers/CategoriesController.kt
@@ -6,6 +6,7 @@ import com.ase.weeshes.data.network.response.CategoriesResponse
 import com.ase.weeshes.domain.model.Category
 import com.ase.weeshes.domain.repository.CategoriesRepository
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.snapshots
 import com.google.firebase.firestore.toObjects
 import kotlinx.coroutines.flow.Flow
@@ -26,7 +27,9 @@ class CategoriesController @Inject constructor(private val firestore: FirebaseFi
     override suspend fun deleteCategory(id: String) = firestore.collection(FirestoreValues.WEESHES_COLL)
         .document(FirestoreValues.USER_ID)
         .collection(FirestoreValues.CATEGORIES_COLL)
-        .document(id).delete().isSuccessful
+        .document(id).delete().isSuccessful.apply {
+            updateProductsCategory(id)
+        }
 
     override suspend fun getCategories(): Flow<List<Category>> = firestore
         .collection(FirestoreValues.WEESHES_COLL)
@@ -36,4 +39,42 @@ class CategoriesController @Inject constructor(private val firestore: FirebaseFi
         .map {
             it.toObjects<CategoriesResponse>().map { w -> w.toDomain() }
         }
+
+    private fun updateProductsCategory(deletedCategory: String?) {
+        if (deletedCategory.isNullOrEmpty()) return
+
+        val ref = firestore
+            .collection(FirestoreValues.WEESHES_COLL)
+            .document(FirestoreValues.USER_ID)
+            .collection(FirestoreValues.WISHLISTS_COLL)
+
+        ref.get().addOnSuccessListener { wishlistsSnapshot ->
+            for (wishlist in wishlistsSnapshot.documents) {
+                val products = wishlist.reference.collection(FirestoreValues.PRODUCTS_COLL)
+
+                products
+                    .whereEqualTo("category", deletedCategory)
+                    .get()
+                    .addOnSuccessListener { productsSnapshot ->
+                        updateProducts(productsSnapshot)
+                    }
+                    .addOnFailureListener { e ->
+                        println("Error getting products: $e")
+                    }
+            }
+        }.addOnFailureListener { e ->
+            println("Error getting wishlists: $e")
+        }
+    }
+
+    private fun updateProducts(product: QuerySnapshot) {
+        val batch = firestore.batch()
+
+        for (productDocument in product.documents) {
+            val productRef = productDocument.reference
+            batch.update(productRef, "category", "")
+        }
+
+        batch.commit().isSuccessful
+    }
 }
--- app/src/main/java/com/ase/weeshes/domain/model/Category.kt
+++ app/src/main/java/com/ase/weeshes/domain/model/Category.kt
@@ -4,9 +4,4 @@ data class Category(
     val id: String,
     val name: String,
     val icon: String,
-) {
-    fun toRequest() = hashMapOf(
-        "name" to name,
-        "icon" to icon
-    )
-}
+)
